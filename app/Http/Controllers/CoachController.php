411d8a7d0af9e729e12c8e9ed030712a86ebe4b2
hshoom17 <hashemalahdal17@gmail.com>
2025-05-22 18:23:06 +0300

    Coach CRUD

--- app/Http/Controllers/CoachController.php
+++ app/Http/Controllers/CoachController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCoachRequest;
+use App\Http\Requests\UpdateCoachRequest;
+use App\Models\Coach;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CoachController extends Controller
-{
-    //
+
+    {
+      public function index()
+    {
+        return Inertia::render('coaches/Index', [
+            'coaches' => Coach::all(),
+        ]);
+    }
+ 
+    public function create()
+    {
+        return Inertia::render('coaches/Create');
+    }
+ 
+    public function store(StoreCoachRequest $request)
+    {
+        Coach::create($request->validated());
+ 
+        return redirect()->route('coaches.index');
+    }
+ 
+    public function edit(Coach $coach)
+    {
+        return Inertia::render('coaches/Edit', [
+            'coach' => $coach,
+        ]);
+    }
+ 
+    public function update(UpdateCoachRequest $request, Coach $coach)
+    {
+        $coach->update($request->validated());
+ 
+        return redirect()->route('coaches.index');
+    }
+ 
+    public function destroy(Coach $coach)
+    {
+        $coach->delete();
+ 
+        return redirect()->route('coaches.index');
+    }
 }
+
+
